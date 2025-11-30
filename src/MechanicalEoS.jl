8fd4498e5e789a47e272f6dd3e56f7f24b8fd133
Albert de Montserrat <albert.de.montserrat@gmail.com>
2025-11-30 11:47:06 +0100

    optimize mechanical EoS kernels

--- src/MechanicalEoS.jl
+++ src/MechanicalEoS.jl
@@ -6,23 +6,23 @@ using MuladdMacro
     K0  = materials.K/1e9
     Kp  = materials.∂K∂P
     Kpp = materials.∂2K∂P2
-    η   = (V0/(V))^(2/3)
+    η   = cbrt((V0/V)^2)# (V0/(V))^(2/3)
     f   = 1/2 * (η - 1.0)
-    P   = 3*K0*f*(1+2*f)^(5/2) * (1 + 3/2*(Kp -4)*f + 3/2*(K0*Kpp + (Kp - 4)*(Kp-3) + 35/9)*f^2 )
+    P   = @muladd 3*K0*f*(1+2*f)^(5/2) * (1 + 3/2*(Kp -4)*f + 3/2*(K0*Kpp + (Kp - 4)*(Kp-3) + 35/9)*f^2 )
     return P
 end
 
-@inline @muladd function mechanical_pressure(model::BM4, V, materials)
+@inline function mechanical_pressure(model::BM4, V, materials)
     # Birch-Murnaghan EOS
     V0  = materials.V0
     K0  = materials.K/1e9
     Kp  = materials.∂K∂P
     Kpp = materials.∂2K∂P2
-    η   = fastpow(V0/V, 2/3)
+    η   = cbrt((V0/V)^2)# (V0/(V))^(2/3)
     f   = 1/2 * (η - 1.0)
-    a   = 1 + 2*f
+    a   = @muladd 1 + 2*f
     b   = 3*K0*f* a * a * sqrt(a)
-    P   = b * (1 + 3/2*(Kp -4)*f + 3/2*(K0*Kpp + (Kp - 4)*(Kp-3) + 35/9)*f*f)
+    P   = @muladd b * (1 + 3/2*(Kp -4)*f + 3/2*(K0*Kpp + (Kp - 4)*(Kp-3) + 35/9)*f*f)
     return P
 end
 
@@ -31,22 +31,22 @@ end
     V0 = materials.V0
     K0 = materials.K/1e9
     Kp = materials.∂K∂P
-    η  = (V0/(V))^(2/3)
+    η   = cbrt((V0/V)^2)# (V0/(V))^(2/3)
     f  = 1/2 * (η - 1.0) 
-    P  = 3*K0*f*(1+2*f)^(5/2) * (1 + 3/2*(Kp - 4)*f )
+    P  = @muladd sqrt(3*K0*f*(1+2*f)^5) * (1 + 3/2*(Kp - 4)*f )
     return P
 end
 
-@inline @muladd function mechanical_pressure(model::BM3, V, materials)
+@inline function mechanical_pressure(model::BM3, V, materials)
     # Birch-Murnaghan EOS
     V0 = materials.V0
     K0 = materials.K/1e9
     Kp = materials.∂K∂P
-    η  = fastpow(V0/V, 2/3)
+    η  = cbrt((V0/V)^2) # fastpow(V0/V, 2/3)
     f  = 1/2 * (η - 1.0) 
-    a  = 1 + 2*f
+    a  = @muladd 1 + 2*f
     b  = 3*K0*f* a * a * sqrt(a)
-    P  = b * (1 + 3/2*(Kp - 4)*f )
+    P  = @muladd b * (1 + 3/2*(Kp - 4)*f )
     return P
 end
 
@@ -54,19 +54,19 @@ end
     # Birch-Murnaghan EOS
     V0 = materials.V0
     K0 = materials.K/1e9
-    η  = (V0/V)^(2/3)
+    η   = cbrt((V0/V)^2)# (V0/(V))^(2/3)
     f  = 1/2 * (η - 1.0) 
-    P  = 3*K0*f*(1 + 2*f)^(5/2)
+    P  = sqrt(3*K0*f*(1 + 2*f)^5)
     return P
 end
 
-@inline @muladd function mechanical_pressure(model::BM2, V, materials)
+@inline function mechanical_pressure(model::BM2, V, materials)
     # Birch-Murnaghan EOS
     V0 = materials.V0
     K0 = materials.K/1e9
-    η  = fastpow(V0/V, 2/3)
+    η  = cbrt((V0/V)^2) # fastpow(V0/V, 2/3)
     f  = 1/2 * (η - 1.0) 
-    a  = 1 + 2*f
+    a  = @muladd 1 + 2*f
     P  = 3*K0*f* a * a * sqrt(a)
     return P
 end
