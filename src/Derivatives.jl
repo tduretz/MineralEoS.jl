9cb99f3ecf5b490e42e2747610a1962fefdf1ba1
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-02-10 16:06:57 +0100

    fix AD bugs

--- src/Derivatives.jl
+++ src/Derivatives.jl
@@ -1,17 +1,17 @@
 @inline function compute_density_derivative(EoS, P, T, params, opts)
-    return value_and_derivative(P -> density_volume(EoS, P, T, params; options = opts), P)
+    return DifferentiationInterface.derivative(P -> density_volume(EoS, P, T, params; options = opts), AutoForwardDiff(), P)[1]
 end
 
 @inline function compute_density_derivative(EoS, P, T, params)
-    return value_and_derivative(P -> density_volume(EoS, P, T, params), P)
+    return DifferentiationInterface.derivative(P -> density_volume(EoS, P, T, params), AutoForwardDiff(), P)[1]
 end
 
 @inline function compute_eff_bulk_modulus(EoS, P, T, params, opts)
-    ρ, ∂ρ∂P = value_and_derivative(P -> density_volume(EoS, P, T, params; options = opts), P)
-    return ρ / ∂ρ∂P
+    f, fprime = value_and_derivative(P -> density_volume(EoS, P, T, params; options = opts), AutoForwardDiff(), P)
+    return f[1] / fprime[1]
 end
 
 @inline function compute_eff_bulk_modulus(EoS, P, T, params)
-    ρ, ∂ρ∂P = value_and_derivative(P -> density_volume(EoS, P, T, params), P)
-    return ρ / ∂ρ∂P
+    f, fprime = value_and_derivative(P -> density_volume(EoS, P, T, params), AutoForwardDiff(), P)
+    return f[1] / fprime[1]
 end
