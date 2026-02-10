19a7070c282b2159432a066aeb6ecf76e3bbc599
tduretz <duretz@uni-frankfurt.de>
2026-02-10 16:45:45 +0100

    new tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 using MineralEoS
 
-@testset "Diamond - simple    " begin
+@testset "Diamond - SimpleEoS()           " begin
     EoS    = SimpleEoS()
     params = assign_EoS_parameters(:Diamond)
     # Low T, low P
@@ -17,7 +17,7 @@ using MineralEoS
     @test V ≈ 3.3889675068800655
     @test ρ ≈ 3547.186562159455
 end
-@testset "Diamond - complex    " begin
+@testset "Diamond - ComplexEoS()          " begin
     EoS    = ComplexEoS()
     params = assign_EoS_parameters(:Diamond)
     # Low T, low P
@@ -34,7 +34,7 @@ end
     @test ρ ≈ 3526.1420454161403
 end
 
-@testset "OlivineFo90 - simple" begin
+@testset "OlivineFo90 - SimpleEoS()       " begin
     EoS    = SimpleEoS()
     params = assign_EoS_parameters(:OlivineFo90)
     # Low T, low P
@@ -51,7 +51,7 @@ end
     @test ρ ≈ 3258.3884577178155
 end
 
-@testset "OlivineFo90 - complex" begin
+@testset "OlivineFo90 - ComplexEoS()      " begin
     EoS    = ComplexEoS()
     params = assign_EoS_parameters(:OlivineFo90)
     # Low T, low P
@@ -66,4 +66,34 @@ end
     ρ, V = density_volume(EoS, P, T, params)
     @test V ≈ 43.258393151067864
     @test ρ ≈ 3246.7225379714073
+end
+
+@testset "Density derivatives             " begin
+    EoS    = ComplexEoS()
+    params = assign_EoS_parameters(:OlivineFo90)
+    # Low T, low P
+    P = 0.0e9
+    T = 300.0
+    dρdP = compute_density_derivative(EoS, P, T, params)
+    @test dρdP ≈ 2.5342682726163707e-8
+end
+
+
+@testset "Eff. bulk modulus - SimpleEoS() " begin
+    EoS    = SimpleEoS()
+    params = assign_EoS_parameters(:OlivineFo90)
+    # Low T, low P
+    P = 0.0e9
+    T = 300.0
+    Keff = compute_eff_bulk_modulus(EoS, P, T, params)
+    @test Keff ≈ params.K
+end
+@testset "Eff. bulk modulus - ComplexEoS()" begin
+    EoS    = ComplexEoS()
+    params = assign_EoS_parameters(:OlivineFo90)
+    # Low T, low P
+    P = 0.0e9
+    T = 300.0
+    Keff = compute_eff_bulk_modulus(EoS, P, T, params)
+    @test Keff ≈ 1.2626240745245195e11
 end
